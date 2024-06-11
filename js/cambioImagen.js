75bfc2bf86613c7d8730eca28f4f6b560ffc3ab8
hapalm <hapalmaz@gmail.com>
2024-06-11 00:31:29 -0300

    onmouseover onmouseout
    Se agrega el comportamiento onmouseover y onmouseout a la imagen del grupo.

---
+++ js/cambioImagen.js
@@ -0,0 +1,8 @@
+
+function cambio(){
+    document.getElementById('imagenGrupo').src = "../imagenes/javascript.jpg";
+}
+
+function original(){
+    document.getElementById('imagenGrupo').src = "../imagenes/imagenGrupo.jpg";
+}
